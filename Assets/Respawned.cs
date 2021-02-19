80ab4a15a14ef9cdb1eae8ff683b01de649f7133
BigBusiness117 <67163482+BigBusiness117@users.noreply.github.com>
2021-02-19 11:18:06 -0800

    killbox,teleporter,checkpoints

---
+++ Assets/Respawned.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Respawned : MonoBehaviour
+{
+
+    public GameObject respawn;
+    public GameObject player;
+    Vector3 respawnPosition;
+    private CharacterController controller;
+    Vector3 myVector;
+    Vector3 nextVector;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        myVector = new Vector3(0f, 0f, 20f);
+        nextVector = new Vector3(0f, 0f, -5f);
+        controller = player.GetComponent<CharacterController>();
+        if (other.tag == "CheckPoint")
+        {
+           
+            respawnPosition = gameObject.transform.position;
+        }
+       
+        else if (other.tag == "KillBox")
+        {
+            player.transform.position = respawnPosition;
+            controller.enabled = false;
+            controller.enabled = true;
+        }
+        if (other.tag == "Teleport")
+        {
+           // controller.enabled = false;
+            gameObject.GetComponent<CharacterController>().Move(myVector);
+           // controller.enabled = true;
+        }
+        if (other.tag == "TeleportBack")
+        {
+            //controller.enabled = false;
+            gameObject.GetComponent<CharacterController>().Move(nextVector);
+           // controller.enabled = true;
+
+        }
+    }
+}
+
